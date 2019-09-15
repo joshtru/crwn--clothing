f4fbf9f5b44896c9e5b8d19fbefaeda44aee45ad
joshtru <joshuaojc@gmail.com>
2019-09-15 12:48:23 -0500

    Implemented sign with google and email using redux-saga

--- src/App.js
+++ src/App.js
@@ -1,72 +1,63 @@
-import React from 'react';
-import { Switch, Route, Redirect } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import { connect } from "react-redux";
+import { createStructuredSelector } from "reselect";
 
-import './App.css';
+import "./App.css";
 
-import Homepage from './pages/homepage/homepage.component';
-import ShopPage from './pages/shop/shop.component';
-import SignInAndSignUp from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import Header from './components/header/header.component';
-import CheckoutPage from './pages/checkout/checkout.component';
+import Homepage from "./pages/homepage/homepage.component";
+import ShopPage from "./pages/shop/shop.component";
+import SignInAndSignUp from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import Header from "./components/header/header.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
 
-import { auth, createUserProfileDocument } from './firebase/firebase.utils';
-
-import { setCurrentUser } from './redux/user/user.actions';
-import { selectCurrentUser } from './redux/user/user.selectors';
+import { selectCurrentUser } from "./redux/user/user.selectors";
 
 class App extends React.Component {
-	unsubsribeFromAuth = null;
+  unsubsribeFromAuth = null;
 
-	componentDidMount() {
-		const { setCurrentUser } = this.props;
+  componentDidMount() {
+    // this.unsubsribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
+    // 	if (userAuth) {
+    // 		const userRef = await createUserProfileDocument(userAuth);
+    // 		userRef.onSnapshot((snapShot) => {
+    // 			setCurrentUser({
+    // 				id: snapShot.id,
+    // 				...snapShot.data()
+    // 			});
+    // 		});
+    // 	}
+    // 	setCurrentUser(userAuth);
+    // });
+  }
 
-		this.unsubsribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-			if (userAuth) {
-				const userRef = await createUserProfileDocument(userAuth);
+  componentWillUnmount() {
+    this.unsubsribeFromAuth();
+  }
 
-				userRef.onSnapshot((snapShot) => {
-					setCurrentUser({
-						id: snapShot.id,
-						...snapShot.data()
-					});
-				});
-			}
-
-			setCurrentUser(userAuth);
-		});
-	}
-
-	componentWillUnmount() {
-		this.unsubsribeFromAuth();
-	}
-
-	render() {
-		return (
-			<div>
-				<Header />
-				<Switch>
-					<Route exact path="/" component={Homepage} />
-					<Route path="/shop" component={ShopPage} />
-					<Route exact path="/checkout" component={CheckoutPage} />
-					<Route
-						exact
-						path="/signin"
-						render={() => (this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />)}
-					/>
-				</Switch>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <Header />
+        <Switch>
+          <Route exact path="/" component={Homepage} />
+          <Route path="/shop" component={ShopPage} />
+          <Route exact path="/checkout" component={CheckoutPage} />
+          <Route
+            exact
+            path="/signin"
+            render={() =>
+              this.props.currentUser ? <Redirect to="/" /> : <SignInAndSignUp />
+            }
+          />
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapStateToProps = createStructuredSelector({
-	currentUser: selectCurrentUser
+  currentUser: selectCurrentUser
 });
 
-const mapDispatchToProps = (disptch) => ({
-	setCurrentUser: (user) => disptch(setCurrentUser(user))
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(mapStateToProps)(App);
