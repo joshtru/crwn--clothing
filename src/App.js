9c0ee85cf375fb9dfd396fde9ace43c9ab30e7c0
joshtru <joshuaojc@gmail.com>
2019-09-16 11:02:27 -0500

    Created user and cart sagas for sign, sign up, and sign in with google

--- src/App.js
+++ src/App.js
@@ -12,23 +12,14 @@ import Header from "./components/header/header.component";
 import CheckoutPage from "./pages/checkout/checkout.component";
 
 import { selectCurrentUser } from "./redux/user/user.selectors";
+import { checkUserSession } from "./redux/user/user.actions";
 
 class App extends React.Component {
   unsubsribeFromAuth = null;
 
   componentDidMount() {
-    // this.unsubsribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
-    // 	if (userAuth) {
-    // 		const userRef = await createUserProfileDocument(userAuth);
-    // 		userRef.onSnapshot((snapShot) => {
-    // 			setCurrentUser({
-    // 				id: snapShot.id,
-    // 				...snapShot.data()
-    // 			});
-    // 		});
-    // 	}
-    // 	setCurrentUser(userAuth);
-    // });
+    const { checkUserSession } = this.props;
+    checkUserSession();
   }
 
   componentWillUnmount() {
@@ -60,4 +51,11 @@ const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser
 });
 
-export default connect(mapStateToProps)(App);
+const mapDispatchToProps = dispatch => ({
+  checkUserSession: () => dispatch(checkUserSession())
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(App);
