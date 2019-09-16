0d5c25ecc02e68edc07c109e03b1d4433d2dedd0
joshtru <joshuaojc@gmail.com>
2019-09-16 18:19:23 -0500

    Updated out application App component, sign-component, and sign-up component using hooks

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -7,8 +7,7 @@ import {
 
 import {
   fetchCollectionsSuccess,
-  fetchCollectionsFailure,
-  fetchCollectionsStart
+  fetchCollectionsFailure
 } from "./shop.actions";
 
 import ShopActionTypes from "./shop.types";
